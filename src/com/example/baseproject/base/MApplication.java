aaaab70b6aa7dde1720e0b6b7efee30fa29026a7
U-Android-PC\Maxitech <Maxitech@Android-PC.(none)>
2013-07-15 15:28:01 +0800

    first commit

---
+++ src/com/example/baseproject/base/MApplication.java
@@ -0,0 +1,17 @@
+package com.example.baseproject.base;
+
+import android.app.Application;
+
+public class MApplication extends Application{
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+		
+		
+		
+	}
+	
+	
+}
